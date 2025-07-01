7dbb8bd9fc29b79106a6b4575474e0dfdbc44f61
itsLeonB <ellionblessan@gmail.com>
2025-07-01 18:31:01 +0700

    fix: use puppeteer for chromium

--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 
+// Set Puppeteer's Chromium executable path to handle environments missing Chromium
+process.env.PUPPETEER_EXECUTABLE_PATH = require('puppeteer').executablePath();
+
 const requiredEnvVars = [
   'NOTION_API_KEY',
   'NOTION_DATABASE_ID',
